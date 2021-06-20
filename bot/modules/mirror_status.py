3d171e2e452ee9e608fa7ad534508e047f21966e
SAF ONE <77989182+AsmSafone@users.noreply.github.com>
2021-06-20 18:29:05 +0600

    Update mirror_status.py

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -12,7 +12,7 @@ import threading
 def mirror_status(update, context):
     message = get_readable_message()
     if len(message) == 0:
-        message = "No Active Downloads! 🤷‍♂️"
+        message = "🤷‍♂️ No Active Downloads!"
         reply_message = sendMessage(message, context.bot, update)
         threading.Thread(target=auto_delete_message, args=(bot, update.message, reply_message)).start()
         return
